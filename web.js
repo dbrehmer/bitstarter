bf42e41d9a9d9230212c395cd0f32494058d7760
Dan Brehmer <dan@brehmer.org>
2013-07-11 06:42:02 +0000

    web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buf = new Buffer(fs.readFileSync('index.html'), "utf-8")
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("ascii"));
+  response.send('buf.toString('ascii')');
 });
 
 var port = process.env.PORT || 5000;
