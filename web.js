0d098145e41ec5496e541c4dba312df32c4fa32d
Dan Brehmer <dan@brehmer.org>
2013-07-11 06:27:36 +0000

    web.js

--- web.js
+++ web.js
@@ -2,8 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(fs.readFileSync('./index.html'), "utf-8")
-
 app.get('/', function(request, response) {
   response.send("Helo World still works!");
 });
